33572f8e863f2d93fd89df417df73967731f1e5d
Shinya Fujino <shf0811@gmail.com>
2021-05-30 00:19:03 +0900

    ch01

---
+++ ch01/list0113.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double x;
+    double y;
+
+    cout << "xとyを加減乗除します。\n";
+
+    cout << "xの値：";
+    cin >> x;
+
+    cout << "yの値：";
+    cin >> y;
+
+    cout << "x + yは" << x + y << "です。\n";
+    cout << "x - yは" << x - y << "です。\n";
+    cout << "x * yは" << x * y << "です。\n";
+    cout << "x / yは" << x / y << "です。\n";
+}
---
+++ ch01/list0118.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name;
+
+    cout << "お名前は：";
+    cin >> name;
+
+    cout << "こんにちは" << name << "さん。\n";
+}
