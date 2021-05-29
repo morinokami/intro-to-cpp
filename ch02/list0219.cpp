622213f5710c74d0158beb5ec213163187163c36
Shinya Fujino <shf0811@gmail.com>
2021-05-30 02:04:28 +0900

    ch02

---
+++ ch02/list0219.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "整数を入力せよ：";
+    cin >> n;
+
+    switch (n) {
+        case 0: cout << "A";
+                cout << "B";    break;
+        case 2: cout << "C";
+        case 5: cout << "D";    break;
+        case 6:
+        case 7: cout << "E";    break;
+        default: cout << "F";   break;
+    }
+    cout << "\n";
+}
