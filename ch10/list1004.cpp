7c2c18c07e2f1d71232441f4bf22157653ab0829
Shinya Fujino <shf0811@gmail.com>
2021-06-06 23:58:36 +0900

    ch10

---
+++ ch10/list1004.cpp
@@ -0,0 +1,51 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Account {
+    string full_name;
+    string number;
+    long crnt_balance;
+
+public:
+    Account(string name, string num, long amnt);
+
+    string name() { return full_name; }
+    string no() { return number; }
+    long balance() { return crnt_balance; }
+
+    void deposit(long amnt);
+    void withdraw(long amnt);
+};
+
+Account::Account(string name, string num, long amnt)
+{
+    full_name = name;
+    number = num;
+    crnt_balance = amnt;
+}
+
+void Account::deposit(long amnt)
+{
+    crnt_balance += amnt;
+}
+
+void Account::withdraw(long amnt)
+{
+    crnt_balance -= amnt;
+}
+
+int main()
+{
+    Account suzuki("鈴木龍一", "12345678", 1000);
+    Account takeda("武田浩文", "87654321", 200);
+
+    suzuki.withdraw(200);
+    takeda.deposit(100);
+
+    cout << "■鈴木君の口座：\"" << suzuki.name() << "\" (" << suzuki.no()
+         << ") " << suzuki.balance() << "円\n";
+    cout << "■武田君の口座：\"" << takeda.name() << "\" (" << takeda.no()
+         << ") " << takeda.balance() << "円\n";
+}
