9aacc8a213cce19238920a6cb6b55b09a023c7f5
Shinya Fujino <shf0811@gmail.com>
2021-06-12 19:11:27 +0900

    DateInit

---
+++ ch11/Date01/Date.h
@@ -0,0 +1,11 @@
+class Date {
+    int y;
+    int m;
+    int d;
+
+public:
+    Date(int yy, int mm, int dd);
+    int year()  { return y; }
+    int month() { return m; }
+    int day()   { return d; }
+};
